02618563d71b961d9ca20feb199eb69619d00757
Jasper Cludts <jasper.cludts@student.kdg.be>
2015-05-18 15:11:59 +0200

    Spel Klaar

--- project/Blu-ray_Disc_Java_Project_SpelMetLetters/src/hellotvxlet/HelloTVXlet.java
+++ project/Blu-ray_Disc_Java_Project_SpelMetLetters/src/hellotvxlet/HelloTVXlet.java
@@ -28,9 +28,9 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
     private HStillImageBackgroundConfiguration bgConfiguration;
     
     private Random gen = new Random();
-    private int r1 = gen.nextInt(13) + 1;
-    private int r2 = gen.nextInt(3) + 1;
-    private int l1, l2;
+    private int r1, r2;
+    private int score;
+    private String l1, l2;
     
     Kaart mc;
     private Graphics g;
@@ -41,7 +41,8 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
     private HBackgroundImage Background = new HBackgroundImage("Background.png");
     
     private HStaticText lblTitel = new HStaticText("Letterspel");
-    private HStaticText lblTekst = new HStaticText("Druk op start om te beginnen");    
+    private HStaticText lblTekst = new HStaticText("Druk op start om te beginnen");
+    private HStaticText lblScore = new HStaticText("");
     
     
     public void initXlet(XletContext context) throws XletStateChangeException {
@@ -51,8 +52,6 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
         sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_DIMENSION, new HScreenDimension(1.0f, 1.0f), HSceneTemplate.REQUIRED);
         sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_LOCATION, new HScreenPoint(0.0f, 0.0f), HSceneTemplate.REQUIRED);
         scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
-        
-       
         
         lblTitel.setLocation(155,50);
         lblTitel.setSize(400,50);
@@ -67,24 +66,29 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
         lblTekst.setBackgroundMode(HVisible.BACKGROUND_FILL);
         lblTekst.setForeground(Color.darkGray);
         
+        lblScore.setLocation(450,5);
+        lblScore.setSize(400,50);
+        lblScore.setBackground(new DVBColor(150,150,150,0));
+        lblScore.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        lblScore.setForeground(Color.darkGray);
+        
       //  l1 = mc.getJuist();
         
-        this.knop1 = new HTextButton("Zwart");    
+        this.knop1 = new HTextButton("De juiste letter");    
         this.knop1.setLocation(150,400);
         this.knop1.setForeground(new DVBColor(255,240,0,255));
         this.knop1.setSize(150,70);
         this.knop1.setBackground(new DVBColor(7,102,0,180));
         this.knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        this.knop1.setActionCommand("Zwart");
            
       //  l2 = mc.getFout();
-        this.knop2 = new HTextButton("Rood");      
+      this.knop2 = new HTextButton("De foute letter");      
       this.knop2.setLocation(380,400);
       this.knop2.setForeground(new DVBColor(255,240,0,255));
       this.knop2.setSize(150,70);
       this.knop2.setBackground(new DVBColor(7,102,0,180));
       this.knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      this.knop2.setActionCommand("Rood");
+      
       
       this.knop3 = new HTextButton("Start");    
         this.knop3.setLocation(270,400);
@@ -116,7 +120,7 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
         
         scene.add(lblTitel);
         scene.add(lblTekst);
-        //scene.add(txtText);
+        scene.add(lblScore);
         bgDevice = screen.getDefaultHBackgroundDevice();
         if(bgDevice.reserveDevice(this)) {
             System.out.println("Background image device has been reserved.");
@@ -141,7 +145,7 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
         Background.load(this);
          r1 = gen.nextInt(25) + 1;
          r2 = gen.nextInt(25) + 1;
-            
+         
         this.mc=new Kaart(r1, r2);
         this.mc.setLocation(305, 225);
         this.mc.setVisible(false);
@@ -188,78 +192,75 @@ public class HelloTVXlet implements Xlet, ResourceClient, HBackgroundImageListen
     
     public void actionPerformed ( ActionEvent e )
     {
-            
         System.out.println(e.getActionCommand());
        
-       if (mc.getLetter(r1,r2)== "Zwart")
-        {
-            if (e.getActionCommand()== "Rood")
-            {
-                this.scene.remove(knop1);
-                this.scene.remove(knop2);
-                this.lblTekst.setTextContent("WINNER", this.lblTekst.NORMAL_STATE);
-                this.mc.setVisible(true);
-                
-            }
-            else
-            {
-                this.scene.remove(knop1);
-                this.scene.remove(knop2);
-                this.lblTekst.setTextContent("LOSER", this.lblTekst.NORMAL_STATE);
-                this.mc.setVisible(true);
-                
-            }
-            
-            this.knop3.setVisible(true);
-        }
-        else
-        {
-            if (e.getActionCommand()=="Zwart")
-            {
-                this.scene.remove(knop1);
-                this.scene.remove(knop2);
-                this.lblTekst.setTextContent("WINNER", this.lblTekst.NORMAL_STATE);
-                this.mc.setVisible(true);
-                
-            }
-            else
-            {
-                this.scene.remove(knop1);
-                this.scene.remove(knop2);
-                this.lblTekst.setTextContent("LOSER", this.lblTekst.NORMAL_STATE);
-                this.mc.setVisible(true);
-                
-            }
-            
-            this.knop3.setVisible(true);
-        }  
         this.knop3.requestFocus();
         
+        l1 = mc.getJuist();
+        l2 = mc.getFout();
+        
+        this.knop1.setActionCommand(l1);
+        this.knop2.setActionCommand(l2);
         
         if (e.getActionCommand() == "Start")
         {
             this.scene.add(knop1);
             this.scene.add(knop2);
             
-            this.mc.paint(g);
-            
-            
-            this.knop1.requestFocus();
+            this.knop1.setTextContent(mc.getJuist(), this.knop1.NORMAL_STATE);
             this.knop1.setVisible(true);
+            
+            this.knop2.setTextContent(mc.getFout(), this.knop2.NORMAL_STATE);
             this.knop2.setVisible(true);
             this.knop3.setVisible(false);
             
-            this.mc.setVisible(false);
-           
+            this.mc.setVisible(true);
             
-            this.lblTekst.setTextContent("Wat is de juiste letter?", this.lblTekst.NORMAL_STATE);
-            
-            
+            this.lblTekst.setTextContent("Wat is de juiste letter?", this.lblTekst.NORMAL_STATE);   
         }
         
+        if (e.getActionCommand() == l1)
+        {
+            this.lblTekst.setTextContent("Beest", this.lblTekst.NORMAL_STATE);
+            this.knop1.setVisible(false);
+            this.knop2.setVisible(false);
+            this.knop3.setVisible(true);
+            this.knop3.requestFocus();
+            
+            r1 = gen.nextInt(25) + 1;
+            r2 = gen.nextInt(25) + 1;
+            System.out.println("random 1 = " + r1);
+            
+            this.scene.remove(mc);
+            this.mc=new Kaart(r1, r2);
+            this.mc.setLocation(305, 225);
+            this.mc.setVisible(false);
+            this.scene.add(mc);
+            this.scene.repaint();
+            score = score + 1;
+        }
         
-        this.scene.repaint();
-             
-             
+        if (e.getActionCommand() == l2)
+        {
+            this.lblTekst.setTextContent("Loser", this.lblTekst.NORMAL_STATE);
+            this.knop1.setVisible(false);
+            this.knop2.setVisible(false);
+            this.knop3.setVisible(true);
+            this.knop3.requestFocus();
+            
+            r1 = gen.nextInt(25) + 1;
+            r2 = gen.nextInt(25) + 1;
+            System.out.println("random 1 = " + r1);
+            
+            this.scene.remove(mc);
+            this.mc=new Kaart(r1, r2);
+            this.mc.setLocation(305, 225);
+            this.mc.setVisible(false);
+            this.scene.add(mc);
+            this.scene.repaint();
+            score = score - 1;
+        }
+        
+        this.lblScore.setTextContent(Integer.toString(score), this.lblTekst.NORMAL_STATE); 
     }
 }
--- project/Blu-ray_Disc_Java_Project_SpelMetLetters/src/hellotvxlet/Kaart.java
+++ project/Blu-ray_Disc_Java_Project_SpelMetLetters/src/hellotvxlet/Kaart.java
@@ -7,69 +7,177 @@ import org.havi.ui.*;
 public class Kaart extends HComponent {
     private Image bmap;
     private MediaTracker mtrack;
-    public int letter1=1;
-    public int letter2=1; 
+    public int letter1;
+    public int letter2; 
     public String kleurTekst;
-    
+    public String letterJuist, letterFout;
     public Kaart(int r1, int r2) {
         
         letter1=r1;
         letter2=r2;
         
-      // bmap = this.getToolkit().getImage("A.png");
+        //juiste letterselectie
         switch(letter1) {
         
             case 1: bmap = this.getToolkit().getImage("A.png");
+            letterJuist = "A";
             break;
             case 2: bmap = this.getToolkit().getImage("B.png");
+            letterJuist = "B";
             break;
             case 3: bmap = this.getToolkit().getImage("C.png");
+            letterJuist = "C";
             break;
             case 4: bmap = this.getToolkit().getImage("D.png");
+            letterJuist = "D";
             break;
             case 5: bmap = this.getToolkit().getImage("E.png");
+            letterJuist = "E";
             break;
             case 6: bmap = this.getToolkit().getImage("F.png");
+            letterJuist = "F";
             break;
             case 7: bmap = this.getToolkit().getImage("G.png");
+            letterJuist = "G";
             break;
             case 8: bmap = this.getToolkit().getImage("H.png");
+            letterJuist = "H";
             break;
             case 9: bmap = this.getToolkit().getImage("I.png");
+            letterJuist = "I";
             break;
             case 10: bmap = this.getToolkit().getImage("J.png");
+            letterJuist = "J";
             break;
             case 11: bmap = this.getToolkit().getImage("K.png");
+            letterJuist = "K";
             break;
             case 12: bmap = this.getToolkit().getImage("L.png");
+            letterJuist = "L";
             break;
             case 13: bmap = this.getToolkit().getImage("M.png");
+            letterJuist = "M";
             break;
             case 14: bmap = this.getToolkit().getImage("N.png");
+            letterJuist = "N";
             break;
             case 15: bmap = this.getToolkit().getImage("O.png");
+            letterJuist = "O";
             break;
             case 16: bmap = this.getToolkit().getImage("P.png");
+            letterJuist = "P";
             break;
             case 17: bmap = this.getToolkit().getImage("Q.png");
+            letterJuist = "Q";
             break;
             case 18: bmap = this.getToolkit().getImage("R.png");
+            letterJuist = "R";
             break;
             case 19: bmap = this.getToolkit().getImage("S.png");
+            letterJuist = "S";
             break;
             case 20: bmap = this.getToolkit().getImage("T.png");
+            letterJuist = "T";
             break;
             case 21: bmap = this.getToolkit().getImage("U.png");
+            letterJuist = "U";
             break;
             case 22: bmap = this.getToolkit().getImage("V.png");
+            letterJuist = "V";
             break;
             case 23: bmap = this.getToolkit().getImage("W.png");
+            letterJuist = "W";
             break;
             case 24: bmap = this.getToolkit().getImage("X.png");
+            letterJuist = "X";
             break;
             case 25: bmap = this.getToolkit().getImage("Y.png");
+            letterJuist = "Y";
             break;
             case 26: bmap = this.getToolkit().getImage("Z.png");
+            letterJuist = "Z";
+            break;
+        }
+            //foute letterselectie
+            switch(letter2) {
+        
+            case 1: 
+            letterFout = "A";
+            break;
+            case 2:
+            letterFout = "B";
+            break;
+            case 3:
+            letterFout = "C";
+            break;
+            case 4:
+            letterFout = "D";
+            break;
+            case 5:
+            letterFout = "E";
+            break;
+            case 6:
+            letterFout = "F";
+            break;
+            case 7:
+            letterFout = "G";
+            break;
+            case 8:
+            letterFout = "H";
+            break;
+            case 9:
+            letterFout = "I";
+            break;
+            case 10:
+            letterFout = "J";
+            break;
+            case 11:
+            letterFout = "K";
+            break;
+            case 12:
+            letterFout = "L";
+            break;
+            case 13:
+            letterFout = "M";
+            break;
+            case 14:
+            letterFout = "N";
+            break;
+            case 15:
+            letterFout = "O";
+            break;
+            case 16:
+            letterFout = "P";
+            break;
+            case 17:
+            letterFout = "Q";
+            break;
+            case 18:
+            letterFout = "R";
+            break;
+            case 19:
+            letterFout = "S";
+            break;
+            case 20:
+            letterFout = "T";
+            break;
+            case 21:
+            letterFout = "U";
+            break;
+            case 22:
+            letterFout = "V";
+            break;
+            case 23:
+            letterFout = "W";
+            break;
+            case 24:
+            letterFout = "X";
+            break;
+            case 25:
+            letterFout = "Y";
+            break;
+            case 26:
+            letterFout = "Z";
             break;
             
         }
@@ -83,37 +191,14 @@ public class Kaart extends HComponent {
         }
         this.setBounds(0,0,100, 155);
     }
-
-    public int getJuist()
-    {
-        return letter1;
-    }
-    public int getFout()
-    {
-        return letter2;
-    }
     
-   public String getLetter(int r1,int r2)
+    public String getJuist()
     {
-        if (letter1 == r1 || letter2 == r2)
-        {
-            kleurTekst = "Zwart";
-        }
-        else
-        {
-            kleurTekst = "Rood";
-        }
-        
-        return kleurTekst;
+        return letterJuist;
     }
-       
-    public void setLetter(int r1, int r2)
+    public String getFout()
     {
-        
-        
-       
-       
-        
+        return letterFout;
     }
     
     public void paint(Graphics g) {
